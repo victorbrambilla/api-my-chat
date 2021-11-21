67b072b6f03e4357feb8f6fe86d2ce366cc21f21
Ubuntu <ubuntu@ip-172-31-13-76.us-east-2.compute.internal>
2021-11-21 18:02:05 +0000

    update

---
+++ build/date.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let getDateTime = (date) => {
+    let today = new Date();
+    let currentDay = today.getDate();
+    let currentMonth = today.getMonth() + 1;
+    let currentYear = today.getFullYear();
+    date = new Date(date);
+    let userDay = date.getDate();
+    let userMonth = date.getMonth() + 1;
+    let userYear = date.getFullYear();
+    let dayMessage;
+    if (userDay == currentDay && userMonth == currentMonth && userYear == currentYear) {
+        dayMessage = 'hoje';
+    }
+    else if (userDay == currentDay - 1 && userMonth == currentMonth && userYear == currentYear) {
+        dayMessage = 'ontem';
+    }
+    else {
+        dayMessage = `${userDay}/${userMonth}/${userYear}`;
+    }
+    let time = {
+        hour: date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds(),
+        msg: dayMessage
+    };
+    return time;
+};
+exports.default = getDateTime;
+//# sourceMappingURL=date.js.map
---
+++ build/models/userModel.js
@@ -0,0 +1,29 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.User = void 0;
+const mongoose_1 = __importDefault(require("mongoose"));
+const userSchema = new mongoose_1.default.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: true
+    }
+});
+const User = mongoose_1.default.model('Users', userSchema);
+exports.User = User;
+//# sourceMappingURL=userModel.js.map
