46f08b51282b9de28e5e84b0456a91f076ac23dc
Victor Brambilla <victor-brambilla@hotmail.com>
2021-11-21 15:42:22 -0400

    update

--- build/libs/bindError.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.error = void 0;
-const error = (res, error) => {
-    return res.status(400).json({ message: error.message });
-};
-exports.error = error;
-//# sourceMappingURL=bindError.js.map
